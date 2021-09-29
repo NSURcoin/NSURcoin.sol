c04e616208653544c3d16c287c2101e6366156d1
NSURcoin <89795835+NSURcoin@users.noreply.github.com>
2021-09-29 15:40:10 +0200

    Update imports.sol

--- imports.sol
+++ imports.sol
@@ -1,7 +1,7 @@
 /**
  * SPDX-License-Identifier: MIT
  */
-pragma solidity ^0.8.4;
+pragma solidity 0.8.4;
 
 interface IERC20 {
     function totalSupply() external view returns (uint256);
@@ -91,25 +91,25 @@ abstract contract Ownable is Context {
         require(_owner == _msgSender(), "Ownable: caller is not the owner");
         _;
     }
-    function renounceOwnership() public virtual onlyOwner {
+    function renounceOwnership() external virtual onlyOwner {
         emit OwnershipTransferred(_owner, address(0));
         _owner = address(0);
     }
-    function transferOwnership(address newOwner) public virtual onlyOwner {
+    function transferOwnership(address newOwner) external virtual onlyOwner {
         require(newOwner != address(0), "Ownable: new owner is the zero address");
         emit OwnershipTransferred(_owner, newOwner);
         _owner = newOwner;
     }
-    function getUnlockTime() public view returns (uint256) {
+    function getUnlockTime() external view returns (uint256) {
         return _lockTime;
     }
-    function lock(uint256 time) public virtual onlyOwner {
+    function lock(uint256 time) external virtual onlyOwner {
         _previousOwner = _owner;
         _owner = address(0);
         _lockTime = block.timestamp + time;
         emit OwnershipTransferred(_owner, address(0));
     }
-    function unlock() public virtual {
+    function unlock() external virtual {
         require(_previousOwner == msg.sender, "Only the previous owner can unlock onwership");
         require(block.timestamp > _lockTime , "The contract is still locked");
         emit OwnershipTransferred(_owner, _previousOwner);
@@ -125,7 +125,7 @@ abstract contract Manageable is Context {
         _manager = msgSender;
         emit ManagementTransferred(address(0), msgSender);
     }
-    function manager() public view returns(address){ return _manager; }
+    function manager() external view returns(address){ return _manager; }
     modifier onlyManager(){
         require(_manager == _msgSender(), "Manageable: caller is not the manager");
         _;
